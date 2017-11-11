135c1db65b69bcedadfc19fcfd10af5a02403d37
Kevin Kelly <kvinklly@gmail.com>
2017-11-11 04:49:37 -0600

    added multi-colored circles

--- circle.js
+++ circle.js
@@ -1,15 +1,26 @@
 const minRad = 5;
 const maxRad = 60;
 const rate = 4;
+const colors = [
+	'#FF0000',
+	'#00FF00',
+	'#0000FF',
+	'#FFFF00',
+	'#FF00FF',
+	'#00FFFF'
+]
 
 function Circle(x, y, r) {
 	this.x = x || random(width);
 	this.y = y || random(height);
 	this.r = r || random(minRad, maxRad);
 	this.alpha = 255 - (this.r / maxRad * 90) - 163;
+	this.c = colors[floor(random(6))];
 
 	this.show = () => {
-		fill(0,255,255,this.alpha);
+		var c = color(this.c)
+		c = changeAlpha(c, this.alpha);
+		fill(c);
     noStroke();
 		ellipse(this.x, this.y, this.r * 2, this.r * 2);
 	}
@@ -22,4 +33,9 @@ function Circle(x, y, r) {
     this.x = random(width);
     this.y = 0 - this.r;
   }
+}
+
+function changeAlpha(col, alp) {
+	col._array[3] = alp / 255;
+	return col;
 }
